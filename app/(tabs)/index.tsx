a619bac3f244558183659ad8fc294a1b74fe8cc3
Matthias Delamare <matthias.delamare@cherbourg.fr>
2024-12-25 16:09:52 +0100

    Premier essai de data fetch

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,19 @@
-import { Text, View, StyleSheet } from "react-native";
+import {Text, View, StyleSheet, Pressable} from "react-native";
 import { Link } from 'expo-router';
 import DayTitle from '@/components/DayTitle';
 import DisplayHour from "@/components/DisplayHour";
 import dayjs, {Dayjs} from "dayjs";
+import {useEffect} from "react";
+
+const url = 'http://localhost/marees.html';
 
 export default function Index() {
+  useEffect(refresh, []);
+  function refresh(){
+    fetch(url, {method: 'get'}).then(resp => {
+      console.log(resp);
+    }).catch(e=>console.log(e));
+  }
   return (
     <View style={styles.container}>
       <DayTitle day={dayjs()} />
@@ -16,6 +25,8 @@ export default function Index() {
       <DisplayHour heure={dayjs()} />
       <DisplayHour heure={dayjs()} actif/>
       <DisplayHour heure={dayjs()} />
+
+      <Pressable onPress={refresh}><Text style={styles.button}>Relancer</Text></Pressable>
     </View>
   );
 }
@@ -33,7 +44,10 @@ const styles = StyleSheet.create({
   },
   button: {
     fontSize: 20,
-    textDecorationLine: 'underline',
+    borderWidth: 2,
+    borderColor: '#ddd',
     color: '#fff',
+    padding: 5,
+    marginTop: 5,
   },
 });
