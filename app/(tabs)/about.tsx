1850ec7cc69ed5c2c82eaa8d725b06b19bc900e6
Matthias Delamare <matthias.delamare@cherbourg.fr>
2025-01-12 19:41:26 +0100

    Affinage des explications sur l'application

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -15,6 +15,10 @@ export default function AboutScreen() {
   const just: StyleProp<TextStyle> = StyleSheet.compose(styles.text,{
     textAlign: 'justify'
   });
+  const left: StyleProp<TextStyle> = StyleSheet.compose(styles.text,{
+    width: '100%',
+    textAlign: 'left'
+  });
 
   useEffect(()=> {
     const subscription = Notifications.addNotificationReceivedListener(displayNotification);
@@ -39,24 +43,24 @@ export default function AboutScreen() {
             </Text>
             <Text style={styles.titre}>A propos de l'application</Text>
             <Text style={just}>
-              L'application envoie
-              une notification lorsqu'une manoeuvre du pont va avoir lieu. Vous pouvez choisir les périodes de temps qui vous intéressent
-              (moments où vous êtes susceptibles de traverser le pont). Vous pouvez aussi choisir combien de temps avant
-              la manoeuvre vous souhaitez être prévenu(e).
+              Si vous avez l'habitude de traverser le pont tournant à certaines heures (pour aller ou revenir du travail par exemple),
+              renseignez ces horaires dans l'application. Vous recevrez une notification quelques minutes avant la manoeuvre.
+              Ce délai est paramétrable à votre guise : vous pouvez définir combien de temps avant que la manoeuvre n'ait lieu
+              vous souhaitez être prévenu.
             </Text>
             <Text style={[just, styles.withSeparator, {borderColor: theme.fg}]}>
               Les informations données ici le sont à titre informative. L'auteur de l'application ne peut être responsable
               de la qualité des prédictions, ni des conséquences de l'utilisation de cette application.
             </Text>
             <Text style={just}>
-              Application développée par Matthias Delamare sous licence Open Source.
+              Application développée par Matthias Delamare.
             </Text>
-            <Text style={styles.text}>
+            <Text style={left}>
               Website <Link href="http://mdelamare.free.fr" style={styles.link}>
                 http://mdelamare.free.fr
               </Link>
             </Text>
-            <Text style={styles.text}>
+            <Text style={left}>
               Github <Link href="https://github.com/ShiftcodeDream/PontTournant" style={styles.link}>
                 https://github.com/ShiftcodeDream/PontTournant
               </Link>
